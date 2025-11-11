3487145c2b3288a08dc601b4e9ad99645f863814
addagirisravani-commits <addagirisravani@gmail.com>
2025-11-11 11:50:45 +0530

    changes

--- src/test/java/com/qa/base/BaseTest.java
+++ src/test/java/com/qa/base/BaseTest.java
@@ -7,28 +7,22 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.time.Duration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-// inside class
-private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
-
-// use
-log.info("Launching browser...");
-
 
 public class BaseTest {
+
     protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        String browser  = ConfigReader.get("browser");
+        String browser   = ConfigReader.get("browser");
         boolean headless = Boolean.parseBoolean(ConfigReader.get("headless"));
+
         DriverFactory.initDriver(browser, headless);
         driver = DriverFactory.getDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(
-                Long.parseLong(ConfigReader.get("implicitWait"))
-        ));
+
+        long implicit = Long.parseLong(ConfigReader.get("implicitWait"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit));
+
         driver.get(ConfigReader.get("baseUrl"));
     }
 
